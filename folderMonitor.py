1d68bc450ea2c12437e629aa9faf47e3085b5c09
anon <anon@anon.anon>
2024-11-02 14:03:48 +0100

    bump; with old script i have had installed for years, no clue why the upstream version was like as it was

--- folderMonitor.py
+++ folderMonitor.py
@@ -1,23 +1,46 @@
-#!/bin/python
+#!/bin/python3
+
+# Periodically display all forders and the number of contents in $PWD
 
 import sys
+import os
+from time import sleep
 import curses
+import signal
 
-def quit():
+def handler(signum, frame):
 	curses.endwin()
+	exit(0)
 
 def main(agv):
-	signal(SIGKILL, quit)
-	signal(SIGTERM, quit)
-	stdscr = curses.initscr()
-	curses.noecho()
-	while true:
-		folders = []
-		os.listdir(".")
-		for i in folders:
-			stdscr.addstr(i)
-		stdscr.refresh()
-		sleep 0.5
+	#try:
+		signal.signal(signal.SIGTERM, handler)
+		stdscr = curses.initscr()
+		curses.noecho()
+		while True:
+			stdscr.clear()
+			folders = os.listdir(".")
+			folders.sort()
+			width = 0
+			for i in folders:
+				if len(i) > width:
+					width = len(i)
+			for i in folders:
+				if not os.path.isdir(i):
+					continue
+				h = os.listdir(i)
+				while len(i) < width:
+					i += " "
+				if not h:
+					stdscr.addstr(i, curses.A_BOLD)
+					stdscr.addstr(" :  [_]\n")
+				else:
+					stdscr.addstr(i, curses.A_BOLD)
+					stdscr.addstr(" :  [X] (" + str(len(h)) + ")\n")
+			stdscr.refresh()
+			sleep(0.5)
+	#except:
+	#	handler(None, None)
 	
 
 if __name__ == '__main__':
